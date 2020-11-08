fd40d870ca314757ca3035b05d2b1ecdb81b0d1f
hadi almuhanna <hadi_almuhanna99@hotmail.com>
2020-11-08 20:27:01 +0200

    Open!!!

---
+++ Alaa Homework/page2.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Alaa_Homework
+{
+    public partial class page2 : System.Web.UI.Page
+    {
+        string a;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            a = Request.QueryString["name"];
+            TextBox1.Text = a;
+
+
+        }
+
+        protected void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+         
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+
+            CheckBox1.Checked = true;
+                }
+    }
+}
